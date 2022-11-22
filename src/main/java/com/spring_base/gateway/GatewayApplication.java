c23318063c503548dfc156edbefa9049f4c38748
newkayak12 <newkayak12@gmail.com>
2022-11-22 22:06:57 +0900

    onlyGateway

--- src/main/java/com/spring_base/gateway/GatewayApplication.java
+++ src/main/java/com/spring_base/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class GatewayApplication {
 
     public static void main(String[] args) {
